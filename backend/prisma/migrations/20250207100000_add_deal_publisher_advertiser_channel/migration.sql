98de12682344a95ead64421d07a0eb3ff0b5e908
alek <ajovicic@arenaentertainment.com>
2026-02-11 04:51:43 +0100

    fix: stable backend version

--- backend/prisma/migrations/20250207100000_add_deal_publisher_advertiser_channel/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable: add publisher_id, advertiser_id, channel_id to deals
-ALTER TABLE "deals" ADD COLUMN "publisher_id" BIGINT NOT NULL DEFAULT 0;
-ALTER TABLE "deals" ADD COLUMN "advertiser_id" BIGINT NOT NULL DEFAULT 0;
-ALTER TABLE "deals" ADD COLUMN "channel_id" BIGINT;
